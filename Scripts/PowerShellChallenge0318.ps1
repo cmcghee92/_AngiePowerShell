6f7b548ebb419b6ed3113810266e8de56ae75d26
AngieK17 <angienkelly@outlook.com>
2026-03-18 10:49:34 -0500

    Added Part 2.

--- Scripts/PowerShellChallenge0318.ps1
+++ Scripts/PowerShellChallenge0318.ps1
@@ -1,8 +1,15 @@
-# Simple Software Inventory Tool
+# Simple Software Inventory Tool-Project 3
 
 $software = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" |
 Where-Object { $_.DisplayName }
 
 $software | Select-Object DisplayName, DisplayVersion, Publisher |
 Sort-Object DisplayName |
+Format-Table -AutoSize
+
+# Service Restart Automation Tool-Project 9
+
+$services = Get-Service | Where-Object { $_.Status -eq "Stopped" }
+
+$services | Select-Object Name, DisplayName, Status |
 Format-Table -AutoSize
